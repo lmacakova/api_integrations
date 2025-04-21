a8a4b5db6a5334e6df3f17d2d4e0b9d22ed59a24
lmacakova <l.macakova.sk@gmail.com>
2025-04-21 01:25:11 +0100

    assignment4

---
+++ assignment04-github.py
@@ -0,0 +1,17 @@
+import requests
+from github import Github
+from config import apikey as cfg # Modules imported
+  
+apikey = cfg["aprivateonekey"] 
+g = Github(apikey)
+
+repo = g.get_repo("lmacakova/aprivateone")
+fileInfo = repo.get_contents("README.md")
+urlOfFile = fileInfo.download_url
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+print (contentOfFile) 
+newContent = "Lucia"
+print (newContent) 
+gitHubResponse=repo.update_file(fileInfo.path,"Assignment4", newContent, fileInfo.sha)
+print (gitHubResponse) 
