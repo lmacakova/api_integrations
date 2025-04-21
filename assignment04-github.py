ecd0599c99d951a644e4e7f5b6a9202dfba79f78
lmacakova <l.macakova.sk@gmail.com>
2025-04-21 02:22:40 +0100

    commit

--- assignment04-github.py
+++ assignment04-github.py
@@ -2,11 +2,11 @@ import requests
 from github import Github
 from config import apiKeys as cfg  # Modules imported
   
-apiKey = cfg["aprivateonekey"] # My private key
+apiKey = cfg ["wsaa.assignments"] # My private key
 g = Github(apiKey)
 
 repo = g.get_repo("lmacakova/wsaa.assignments") # Chosen repository
-fileInfo = repo.get_contents("text.txt") # Chosen file
+fileInfo = repo.get_contents("README.md") # Chosen file
 urlOfFile = fileInfo.download_url
 response = requests.get(urlOfFile)
 contentOfFile = response.text
@@ -14,5 +14,5 @@ print (contentOfFile) # Current content
 newContent = "Lucia"  # New content
 print (newContent)
 gitHubResponse=repo.update_file(fileInfo.path,"Assignment4", newContent, fileInfo.sha)
-print (gitHubResponse) 
+#print (gitHubResponse) 
 
